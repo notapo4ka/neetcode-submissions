4131fcd6311b5b02efdd0ddc3c1af3a4208e85fe
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2026-07-29 21:44:27 +0300

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.java
@@ -0,0 +1,33 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (Integer.compare(minStack.peek(), stack.peek()) == 0) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
