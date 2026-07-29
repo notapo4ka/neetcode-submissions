15ba807bf480bc769277187509947185cde76c92
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2026-07-29 21:18:55 +0300

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.isEmpty()) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty()) {
+                    if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }   
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+}
