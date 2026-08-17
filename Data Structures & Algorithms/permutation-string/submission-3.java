ee9d6dee200c473c2840540a95aaa9498a955a11
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2026-08-17 15:50:32 +0300

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length() || s2.length() == 0) {
+            return false;
+        }
+
+        int x = s1.length();
+        int y = s2.length();
+
+        int[] array1 = new int[26];
+        int[] array2 = new int[26];
+
+        for (int i = 0; i < x; i++) {
+            array1[s1.charAt(i) - 'a']++;
+            array2[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = x; i < y; i++) {
+            if (Arrays.equals(array1, array2)) {
+                return true;
+            }
+
+            array2[s2.charAt(i - x) - 'a']--;
+            array2[s2.charAt(i) - 'a']++;
+        }
+
+        return Arrays.equals(array1, array2);
+    }
+}
